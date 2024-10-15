4d3916b03109c645a074ec034ac7c9ddbcfd1ce0
mike283 <emichael283@gmail.com>
2024-10-15 15:17:43 +0200

    Changed folders names

--- DLX_processorSYNTHESIS/compile0.tcl
+++
@@ -1,8 +0,0 @@
-set_wire_load_model -name 5K_hvratio_1_4
-compile -exact_map
-report_power > POWER_REPORTS/optimised0_power.rpt
-report_timing > TIMING_REPORTS/optimised0_timing.rpt
-report_area > AREA_REPORTS/optimised0_area.rpt
-write -hierarchy -f ddc -output dlx_optimised0_syn.ddc
-write -hierarchy -f verilog -output dlx_optimised0.v
-
--- DLX_processorSYNTHESIS/compile2.tcl
+++
@@ -1,8 +0,0 @@
-set_wire_load_model -name 5K_hvratio_1_4
-compile_ultra -gate_clock
-report_power > POWER_REPORTS/optimised2_power.rpt
-report_timing > TIMING_REPORTS/optimised2_timing.rpt
-report_area > AREA_REPORTS/optimised2_area.rpt
-write -hierarchy -f ddc -output dlx_optimised2_syn.ddc
-write -hierarchy -f verilog -output dlx_optimised2.v
-
